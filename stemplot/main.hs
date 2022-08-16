e8837599cc391999d14cce8f083c2cd332e7016d
Ethan Evans <ethanalexevans@gmail.com>
2022-08-16 13:33:53 -0700

    stemplot example

---
+++ stemplot/main.hs
@@ -0,0 +1,33 @@
+module Main where
+
+import Data.List
+import Text.PrettyPrint
+
+nums :: [Int]
+nums = [ 12, 32, 65, 61, 82, 66,  3, 64, 52
+       , 84, 10, 75, 37, 77, 65, 56, 68, 10
+       , 47, 42, 72, 49, 55, 48, 32, 44, 32 ]
+
+sorted :: [Int]
+sorted = sort nums
+
+stem :: Int -> Int
+stem x = x `div` 10
+
+leaf :: Int -> Int
+leaf x = x `mod` 10
+
+stems :: [Int] -> [Int]
+stems xs = map stem xs
+
+leaves :: [Int] -> [Int]
+leaves xs = map leaf xs
+
+stemplot :: [Int] -> [(Int, [Int])]
+stemplot xs = map (\x -> (x, leaves $ filter (\y -> stem y == x) xs)) (nub $ stems xs)
+
+plot :: [(Int, [Int])] -> IO ()
+plot plt = putStrLn $ render $ vcat $ map (\(x, y) -> text (show x) <+> text (show y)) plt
+
+main :: IO ()
+main = undefined
