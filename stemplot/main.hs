5d92a4247af8a6dc3d7b4f7ab6babc39b7709389
Ethan Evans <ethanalexevans@gmail.com>
2022-08-17 11:06:33 -0700

    simplify stemplot example

--- stemplot/main.hs
+++ stemplot/main.hs
@@ -3,13 +3,13 @@ module Main where
 import Data.List
 import Text.PrettyPrint
 
+type Stem = Int
+type Leaf = Int
+
 nums :: [Int]
-nums = [ 12, 32, 65, 61, 82, 66,  3, 64, 52
+nums = sort [ 12, 32, 65, 61, 82, 66,  3, 64, 52
        , 84, 10, 75, 37, 77, 65, 56, 68, 10
        , 47, 42, 72, 49, 55, 48, 32, 44, 32 ]
-
-sorted :: [Int]
-sorted = sort nums
 
 stem :: Int -> Int
 stem x = x `div` 10
@@ -23,10 +23,10 @@ stems xs = map stem xs
 leaves :: [Int] -> [Int]
 leaves xs = map leaf xs
 
-stemplot :: [Int] -> [(Int, [Int])]
+stemplot :: [Int] -> [(Stem, [Leaf])]
 stemplot xs = map (\x -> (x, leaves $ filter (\y -> stem y == x) xs)) (nub $ stems xs)
 
-plot :: [(Int, [Int])] -> IO ()
+plot :: [(Stem, [Leaf])] -> IO ()
 plot plt = putStrLn $ render $ vcat $ map (\(x, y) -> text (show x) <+> text (show y)) plt
 
 main :: IO ()
